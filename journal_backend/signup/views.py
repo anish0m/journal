d87db403aebd306c83231b8efd4fbd683dcdc0cf
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-05-19 18:34:22 +0600

    Add verify-OTP view

--- journal_backend/signup/views.py
+++ journal_backend/signup/views.py
@@ -6,6 +6,7 @@ from .serializers import SignupSerializer
 import random
 from django.core.mail import send_mail
 from django.conf import settings
+from django.contrib.auth.models import User
 
 
 class SignupView(APIView):
@@ -25,3 +26,27 @@ class SignupView(APIView):
             )
             return Response({'message': 'OTP sent to email'}, status=201)
         return Response(serializer.errors, status=400)
+
+class VerifyOTPView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        otp = request.data.get('otp')
+
+        try:
+            pending = PendingUser.objects.get(email=email)
+            if pending.otp == otp:
+                # Create real User
+                user = User.objects.create_user(
+                    username=pending.user_name,
+                    email=pending.email,
+                    password=pending.password,
+                    first_name=pending.first_name,
+                    last_name=pending.last_name
+                )
+                # Profile pic can be handled later
+                pending.delete()
+                return Response({'message': 'Account created'}, status=201)
+            else:
+                return Response({'error': 'Invalid OTP'}, status=400)
+        except PendingUser.DoesNotExist:
+            return Response({'error': 'Email not found'}, status=404)
