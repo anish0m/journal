b11c1baf9f21ea5d9cb0b43ef285b91df4bfe2b4
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-05-19 17:53:16 +0600

    Add user model

--- journal_backend/signup/models.py
+++ journal_backend/signup/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class PendingUser(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    user_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    profile_picture = models.ImageField(
+        upload_to='', blank=True, null=True)
+    password = models.CharField(max_length=100)
+    otp = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
