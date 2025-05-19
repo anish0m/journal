54c6601c31c7e06222877a455102d055d43f4277
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-05-19 20:02:02 +0600

    URL correction

--- journal_backend/journal_backend/urls.py
+++ journal_backend/journal_backend/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("login.urls")),
+    path("signup/", include("signup.urls")),
     path("profile/", include("userprofile.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
