2e83a4542c2847e465ca0b2e4510349c8a8ccf82
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-09-05 12:46:48 +0600

    backend: Implement ProfileView for retrieving and updating user profiles

--- journal_backend/accounts/views.py
+++ journal_backend/accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from .models import Profile
+from .serializers import UserSerializer, ProfileSerializer
 
-# Create your views here.
+class ProfileView(generics.RetrieveUpdateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_object(self):
+        return self.request.user.profile
