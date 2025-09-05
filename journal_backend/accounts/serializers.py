48c7de647798a2c1c440bc81ce724c54a99fb74c
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-09-05 12:46:02 +0600

    backend: Implement serializers for User and Profile models

---
+++ journal_backend/accounts/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio', 'created_at', 'updated_at']
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+    
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'profile']
+        extra_kwargs = {'password': {'write_only': True}}
