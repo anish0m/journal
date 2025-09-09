8762253e0bfa3bf81894aad3b2c95c41c839eff3
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-09-09 15:54:25 +0600

    backend: Update ProfileSerializer to include additional user fields and modify UserSerializer structure

--- journal_backend/accounts/serializers.py
+++ journal_backend/accounts/serializers.py
@@ -2,15 +2,31 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Profile
 
+
 class ProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    email = serializers.EmailField(source='user.email', read_only=True)
+    first_name = serializers.CharField(
+        source='user.first_name', read_only=True)
+    last_name = serializers.CharField(source='user.last_name', read_only=True)
+
     class Meta:
         model = Profile
-        fields = ['avatar', 'bio', 'created_at', 'updated_at']
+        fields = [
+            'id',
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'title',
+            'mobile',
+            'address',
+            'social_links',
+            'avatar'
+        ]
+
 
 class UserSerializer(serializers.ModelSerializer):
-    profile = ProfileSerializer(read_only=True)
-    
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'profile']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
