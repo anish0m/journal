4b36cbbfe1fe8b2c806ecc00e7a20c3e64a3eea1
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-09-09 16:02:51 +0600

    backend: Implement JournalEntryListCreateView for managing journal entries

--- journal_backend/journal/views.py
+++ journal_backend/journal/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from .models import JournalEntry
+from .serializers import JournalEntrySerializer
 
 # Create your views here.
+
+
+class JournalEntryListCreateView(generics.ListCreateAPIView):
+    serializer_class = JournalEntrySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return JournalEntry.objects.filter(author=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
